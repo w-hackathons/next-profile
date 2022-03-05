333ef9b0ded683aec1c5daf3b5ba6f3f56c9952f
RimlTempest <riml.slime@gmail.com>
2022-03-05 17:04:16 +0900

    fix: prefix

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   reactStrictMode: true,
-}
+  assetPrefix: process.env.NODE_ENV === "production" ? "/next-profile" : "",
+
+  publicRuntimeConfig: {
+    basePath: process.env.NODE_ENV === "production" ? "/next-profile" : "",
+  }
+};
