89ebb38c96b888af8f89516318e746997c06e7b5
RimlTempest <RimlTempest@users.noreply.github.com>
2022-03-05 12:09:46 +0000

    deploy: aa4e31dfd7f63c20a7906ee317414ddaa4912cdd

---
+++ _next/static/dIMGfmy_KGggnYmqHzKkg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/4c6acd6328cfc12b.css","static/chunks/pages/index-d4093f41f06a9529.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
